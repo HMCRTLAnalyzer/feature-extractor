1fdbc796c3849541129af8c30c618a1049331086
slmnemo <nicholas.lucioforlife@yahoo.com>
2024-03-18 19:03:53 +0000

    Added csv output for features, Compatability update for new library changes

--- AIGERanalyzer.py
+++ AIGERanalyzer.py
@@ -5,18 +5,34 @@ from aigerdotlib import *
 import numpy as np
 import pandas as pd
 import pickle
+import argparse
 
-csv_filepath = "diffs_test_paths.csv"
+
+
+
+
+csv_in_filepath = "diffs_test_paths.csv"
+csv_out_filepath = "teststats.csv"
 pickle_dirpath = "./graph_pickles"
 regenerate_pickles = 0
-openABCD_df = pd.read_csv(csv_filepath)
-openABCD_df = openABCD_df.loc[:,["module","path_to_rtl","language"]]
+openABCD_df = pd.read_csv(csv_in_filepath)
+openABCD_df = openABCD_df.loc[:,["module","path_to_rtl","language","sensitive","memory"]]
 print(openABCD_df)
 
+# Define Parameters for dataframe and analysis
 IO_basenames = ["I","O","L"]
 output_basenames = ["O","L"]
+N = 5
 
-stats_df = pd.DataFrame()
+nlist = list(range(N))
+nlist.sort(reverse=True)
+ld_cols = [f"LD{x}" for x in nlist]
+le_cols = [f"LE{x}" for x in nlist]
+nodecnt_cols = [f"NCNT{x}" for x in nlist]
+fanout_cols = [f"FO{x}" for x in nlist]
+
+stats_col = ["module","sensitive","memory"]+ld_cols+le_cols+nodecnt_cols+fanout_cols
+df_list = []
 
 graph_dict = {}
 graph_dict["module"] = []
@@ -25,48 +41,49 @@ for row in openABCD_df.itertuples(index=False):
     module = row[0]
     path_to_rtl = row[1]
     language = row[2]
+    sensitive = row[3]
+    memory = row[4]
     graph_filepath = f"{pickle_dirpath}/{module}_graphs.pickle"
-    input_filepath = f"{pickle_dirpath}/{module}_inputs.pickle"
-    output_filepath = f"{pickle_dirpath}/{module}_outputs.pickle"
     
     # Check if pickle exists. if it does, load the pickle instead of regenerating graph list.
     try:
         if regenerate_pickles == 1:
             dot_filepath = generateDOT(path_to_rtl, module, language)
             print(f"Generating pickle for {module}")
-            [graph_list, output_names, input_names] = cutAIGERtoDAGs(dot_filepath, IO_basenames, output_basenames)
-            for graph in graph_list:
-                NotEdgetoNotNode(graph)
+            graph_dict = cutAIGERtoDAGs(dot_filepath, IO_basenames, output_basenames)
+            for key, entry in graph_dict.items():
+                graph = entry["graph"]
+                replaceEdgesWithNotNodes(graph)
+                replaceBufWithLatch(graph)
+                logicalEffortEdgeMap(graph)
             with open(graph_filepath, "wb") as handle:
-                pickle.dump(graph_list, handle)
-            with open(input_filepath, "wb") as handle:
-                pickle.dump(input_names, handle)
-            with open(output_filepath, "wb") as handle:
-                pickle.dump(output_names, handle)
-
+                pickle.dump(graph_dict, handle)
+        
         graph_file = open(graph_filepath,"rb")
-        graph_list = pickle.load(graph_file)
-        input_file = open(input_filepath,"rb")
-        input_names = pickle.load(input_file)
-        output_file = open(output_filepath,"rb")
-        output_names = pickle.load(output_file)
+        graph_dict = pickle.load(graph_file)
     except:
         dot_filepath = generateDOT(path_to_rtl, module, language)
         print(f"Generating pickle for {module}")
-        [graph_list, output_names, input_names] = cutAIGERtoDAGs(dot_filepath, IO_basenames, output_basenames)
-        for graph in graph_list:
-            NotEdgetoNotNode(graph)
+        graph_dict = cutAIGERtoDAGs(dot_filepath, IO_basenames, output_basenames)
+        idx = 0
+        for key, entry in graph_dict.items():
+            graph = entry["graph"]
+            replaceEdgesWithNotNodes(graph)
+            replaceBufWithLatch(graph)
+            logicalEffortEdgeMap(graph)
+            nx.nx_pydot.write_dot(graph,f"{idx}.dot")
+            idx += 1
         with open(graph_filepath, "wb") as handle:
-            pickle.dump(graph_list, handle)
-        with open(input_filepath, "wb") as handle:
-            pickle.dump(input_names, handle)
-        with open(output_filepath, "wb") as handle:
-            pickle.dump(output_names, handle)
+            pickle.dump(graph_dict, handle)
 
-    graph_dict["module"] = graph_list
+    LD = topNLargestLD(graph_dict,N)
+    LE = topNLargestLE(graph_dict,N)
+    LNC = topNCLBNodeCount(graph_dict,N)
+    FN = topNFanouts(graph_dict,N)
+    vals = [module, sensitive, memory]+LD+LE+LNC+FN
 
-    print("PYTHON OUTPUT")
-    print()
-    print([n for n in graph_list[0].nodes()])
+    df_list += [dict(zip(stats_col,vals))]
 
-
+# After getting all df items, create dataframe and save it to a csv.
+stats_df = pd.DataFrame(data=df_list)
+stats_df.to_csv(csv_out_filepath)
