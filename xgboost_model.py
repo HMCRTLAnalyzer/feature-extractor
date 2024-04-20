7783e61f7c028cb46e7493d31ae2ee6de7fc776d
esundheim <esundheim@hmc.edu>
2024-04-20 19:37:17 +0000

    Added top N features(hardcoded) and normalized  logical effort

--- xgboost_model.py
+++ xgboost_model.py
@@ -9,14 +9,28 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
 # load data as numpy array
-dataset = loadtxt('/home/qualcomm_clinic/RTL_dataset/temp_top350.csv', delimiter=",", skiprows = 1, dtype="str")
+file = 'test4_15.csv'
+if (file == "test4_15.csv"):
+    print("using new data")
+    start_of_data = 5
+    sens_col = 3
+    mem_col = 4
+else:
+    start_of_data = 3
+    sens_col = 1
+    mem_col = 2
+#dataset = loadtxt('/home/qualcomm_clinic/RTL_dataset/temp_top350.csv', delimiter=",", skiprows = 1, dtype="str")
+dataset = loadtxt('test4_15.csv', delimiter=",", skiprows = 1, dtype="str")
 
 # split data into X and y
-X = dataset[:,3:]
+X = dataset[:, start_of_data:]
 X[:, -1] = [name.strip("[]") for name in X[:, -1]]
-Y = dataset[:,0:2] # columns in order: names, sensitivity, memory
+Y = dataset[:, 0:start_of_data] # columns in order: names, sensitivity, memory OR names, delay delta , area delta, sens, mem
 Y[:, 0] = [name.strip("[]") for name in Y[:, 0]]
 
+# ----------------------------------------
+# to do: try training on only non-memories! -diego's suggestion
+# ----------------------------------------
 # split data into train and test sets
 seed = 7
 test_size = 0.3
@@ -24,17 +38,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, r
 
 #to keep labels with data, pull off only after splitting
 names_train = y_train[:, 0]
-sens_train = y_train[:, 1]
-mem_train = y_train[:, 2]
+sens_train = y_train[:, sens_col]
+mem_train = y_train[:, mem_col]
 names_test = y_test[:, 0]
-sens_test = y_test[:, 1]
-mem_test = y_test[:, 2]
+sens_test = y_test[:, sens_col]
+mem_test = y_test[:, mem_col]
 
 #clean up data now
-X_train = X_train[:, 3:].astype(np.float64) #start at 3rd index and drop the last one
-X_test = X_test[:, 3:].astype(np.float64)
-y_train = y_train[:, 1].astype(np.float64) #label = sensitivity so column 1
-y_test = y_test[:, 1].astype(np.float64)
+X_train = X_train[:, start_of_data:].astype(np.float64)
+X_test = X_test[:, start_of_data:].astype(np.float64)
+y_train = y_train[:, sens_col].astype(np.float64) #label = sensitivity so column 1
+y_test = y_test[:, sens_col].astype(np.float64)
+
+#cleaning up data to try fewer columns
+num_features = 350
+X_train = np.concatenate((X_train[:, start_of_data:start_of_data+num_features], X_train[:, start_of_data+500: start_of_data+500+num_features], X_train[:, start_of_data+1000: start_of_data+1000+num_features], X_train[:, start_of_data+1500: start_of_data+1500+num_features], X_train[:,start_of_data+2000:start_of_data+2000+num_features]), axis = 1)
+print(X_train)
+X_test = np.concatenate((X_test[:, start_of_data:start_of_data+num_features], X_test[:, start_of_data+500: start_of_data+500+num_features], X_test[:, start_of_data+1000: start_of_data+1000+num_features], X_test[:, start_of_data+1500: start_of_data+1500+num_features], X_test[:,start_of_data+2000:start_of_data+2000+num_features]), axis = 1)
 
 # fit model to training data
 model = XGBClassifier()
@@ -43,8 +63,13 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 predictions = [round(value) for value in y_pred]
 
-misclassified = [names_test[i] for i in range(len(names_test)) if predictions[i] != y_test[i]]
-print(misclassified)
+# error analysis - which ones are still being misclassified?
+# false_pos = [names_test[i] for i in range(len(names_test)) if predictions[i] > y_test[i]]
+# false_neg = [names_test[i] for i in range(len(names_test)) if predictions[i] < y_test[i]]
+# print("False positives: \n")
+# print(*false_pos, sep ='\n')
+# print("False negatives: \n")
+# print(*false_neg, sep ='\n')
 
 # evaluate predictions
 accuracy = metrics.accuracy_score(y_test, predictions)
@@ -61,8 +86,8 @@ cm_display.plot()
 plt.savefig("xgboost_confusion_matrix.png")
 
 #save images
-'''
+
 a = xgboost.plot_tree(model, num_trees = 1)
-plt.savefig("xbg_tree.png", dpi = 600)
+#plt.savefig("xbg_tree.png", dpi = 600)
 b = xgboost.plot_importance(model, max_num_features=20)
-plt.savefig("xbg_importance.png", dpi = 600)'''
+#plt.savefig("xbg_importance_" + str(num_features) + ".png", dpi = 600)
